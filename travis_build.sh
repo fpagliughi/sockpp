664a61fa0417e0c0a81953091d1c196e79a4700c
fmp <fpagliughi@mindspring.com>
2018-12-03 16:42:51 -0500

    Updated the travis build script to run the unit tests.

--- travis_build.sh
+++ travis_build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# travis_build.sh
+#
+# Travis CI build/test script for the sockpp library.
+#
 
 set -e
 
@@ -6,8 +11,11 @@ rm -rf build
 mkdir build
 cd build
 echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
-cmake -DSOCKPP_BUILD_DOCUMENTATION=OFF -DSOCKPP_BUILD_EXAMPLES=ON ..
-make
+cmake -DSOCKPP_BUILD_EXAMPLES=ON -DSOCKPP_BUILD_TESTS .. && \
+  make && \
+  ./tests/unit/unit_tests --success
+
+#ctest -VV --timeout 600
 #cpack --verbose
 
 
