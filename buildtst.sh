d6fb0ae0ba863fb2b837c792dae91c6e7a96d174
fmp <fpagliughi@mindspring.com>
2018-12-02 22:37:22 -0500

    Updated buildtst.sh to use CMake

--- buildtst.sh
+++ buildtst.sh
@@ -3,19 +3,25 @@
 # Test the build with a few compilers.
 #
 
-for COMPILER in g++-4.9 g++-5 g++-6 clang++-3.8
+for COMPILER in g++-4.9 g++-5 g++-6 g++-7 g++-8 clang++-3.8 clang++-4.0 clang++-5.0 clang++-6.0
 do
-    if [ -z "$(which ${COMPILER})" ]; then
-	printf "Compiler not found: %s\n" "${COMPILER}"
-    else
-	printf "Testing: %s\n" "${COMPILER}"
-	make distclean
-	if ! make CXX=${COMPILER} ; then
-	    printf "\nCompilation failed for %s\n" "${COMPILER}"
-	    exit 1
-	fi
+  if [ -z "$(which ${COMPILER})" ]; then
+    printf "Compiler not found: %s\n" "${COMPILER}"
+  else
+    printf "Testing: %s\n" "${COMPILER}"
+    rm -rf build/
+    mkdir build ; pushd build
+    if ! cmake -DSOCKPP_BUILD_EXAMPLES=ON .. ; then
+      printf "\nCMake configuration failed for %s\n" "${COMPILER}"
+	  exit 1
     fi
-    printf "\n"
+    if ! make -j8 CXX=${COMPILER} ; then
+	  printf "\nCompilation failed for %s\n" "${COMPILER}"
+	  exit 1
+	fi
+    popd
+  fi
+  printf "\n"
 done
 
 make distclean
