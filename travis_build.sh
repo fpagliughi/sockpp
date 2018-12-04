22f71d6779140f961f45c9c04b7a7add4f56f0a4
fmp <fpagliughi@mindspring.com>
2018-12-04 07:53:36 -0500

    Split out travis_install_catch2.sh into separate script

--- travis_build.sh
+++ travis_build.sh
@@ -7,24 +7,13 @@
 
 set -e
 
-# Install Catch2 from sources
-which cmake
-wget https://github.com/catchorg/Catch2/archive/v2.5.0.tar.gz
-tar -xf v2.5.0.tar.gz
-pushd Catch2-2.5.0/
-cmake -Bbuild -H. -DBUILD_TESTING=OFF
-sudo env "PATH=$PATH" cmake --build build/ --target install
-popd
-
-rm -rf build
-mkdir build
-cd build
 echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
-cmake -DSOCKPP_BUILD_EXAMPLES=ON -DSOCKPP_BUILD_TESTS=ON .. && \
-  make && \
-  ./tests/unit/unit_tests --success
+cmake -Bbuild -H. -DSOCKPP_BUILD_EXAMPLES=ON -DSOCKPP_BUILD_TESTS=ON
+cmake --build build/
+
+# Run the unit tests
+./build/tests/unit/unit_tests --success
 
 #ctest -VV --timeout 600
 #cpack --verbose
-
 
