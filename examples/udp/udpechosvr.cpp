84f8f2cdbed08f9f4f159eeda9943c5152574a06
Ilya Tsybulsky <ilya.tsybulsky@gmail.com>
2020-03-25 15:08:24 +0300

    Fixed broken compilation of examples on Windows/msvs2015u3

--- examples/udp/udpechosvr.cpp
+++ examples/udp/udpechosvr.cpp
@@ -72,7 +72,7 @@ void run_echo(UDPSOCK sock)
 	// Read some data, also getting the address of the sender,
 	// then just send it back.
 	while ((n = sock.recv_from(buf, sizeof(buf), &srcAddr)) > 0)
-		sock.send_to(buf, n, srcAddr);
+		sock.send_to((void*)buf, n, srcAddr);
 }
 
 // --------------------------------------------------------------------------
