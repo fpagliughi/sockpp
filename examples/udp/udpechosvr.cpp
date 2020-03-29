a42a4008ccf0bcc416ad0e9d9d23c38fdcda6827
fpagliughi <fpagliughi@mindspring.com>
2020-03-29 10:11:07 -0400

    Restored C buf to UDB echo server example.

--- examples/udp/udpechosvr.cpp
+++ examples/udp/udpechosvr.cpp
@@ -47,7 +47,6 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // --------------------------------------------------------------------------
 
-#include <array>
 #include <iostream>
 #include <thread>
 #include "sockpp/udp_socket.h"
@@ -65,15 +64,15 @@ template <typename UDPSOCK>
 void run_echo(UDPSOCK sock)
 {
 	ssize_t n;
-	std::array<uint8_t, 512> buf;
+	char buf[512];
 
 	// Each UDP socket type knows its address type as `addr_t`
 	typename UDPSOCK::addr_t srcAddr;
 
 	// Read some data, also getting the address of the sender,
 	// then just send it back.
-	while ((n = sock.recv_from(buf.data(), buf.size(), &srcAddr)) > 0)
-		sock.send_to(buf.data(), n, srcAddr);
+	while ((n = sock.recv_from(buf, sizeof(buf), &srcAddr)) > 0)
+		sock.send_to(buf, n, srcAddr);
 }
 
 // --------------------------------------------------------------------------
